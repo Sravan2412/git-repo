677bdcdaf0864bf35807d20688809bc6a2b263f9
saravanan <saradhan24@gmail.com>
2021-10-23 18:13:12 +0530

    update

--- samplepython.py
+++ samplepython.py
@@ -1,4 +1,4 @@
-import os
+
 list1 = [1, 3, 5, 4, 2]
 list1.sort()
 print('The sorted list is: {0}'.format(list1))
@@ -12,3 +12,24 @@ print('The copied list is: ', list2)
 
 indexvalue = list2[2:5]
 print('The index values are: ', indexvalue)
+
+
+user_value = int(input('Enter a number: '))
+num1 = 0
+num2 = 1
+count = 0
+
+if(user_value <= 0):
+    print('Please enter a positive number')
+
+elif(user_value == 1):
+    print(user_value)
+
+else:
+    print('Fibonacci sequence: ')
+    while count < user_value:
+        print(num1)
+        num3 = num1+num2
+        num1 = num2
+        num2 = num3
+        count += 1
