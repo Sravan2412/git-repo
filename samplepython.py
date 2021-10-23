9cd763086b5561ebf021afdfc3d1ca5b694d67b6
saravanan <saradhan24@gmail.com>
2021-10-23 18:33:58 +0530

    update

--- samplepython.py
+++ samplepython.py
@@ -14,22 +14,4 @@ indexvalue = list2[2:5]
 print('The index values are: ', indexvalue)
 
 
-user_value = int(input('Enter a number: '))
-num1 = 0
-num2 = 1
-count = 0
-
-if(user_value <= 0):
-    print('Please enter a positive number')
-
-elif(user_value == 1):
-    print(user_value)
-
-else:
-    print('Fibonacci sequence: ')
-    while count < user_value:
-        print(num1)
-        num3 = num1+num2
-        num1 = num2
-        num2 = num3
-        count += 1
+print('Python program is successfully executed')
