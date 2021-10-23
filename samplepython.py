bedafa0bdb950c7e302a9e0256ea92c5c9f9ef6c
saravanan <saradhan24@gmail.com>
2021-10-23 17:18:13 +0530

    added python file

---
+++ samplepython.py
@@ -0,0 +1,14 @@
+import os
+list1 = [1, 3, 5, 4, 2]
+list1.sort()
+print('The sorted list is: {0}'.format(list1))
+
+list1.reverse()
+print('The reversed list is: {0}'.format(list1))
+
+list2 = []
+list2 = list1.copy()
+print('The copied list is: ', list2)
+
+indexvalue = list2[2:5]
+print('The index values are: ', indexvalue)
